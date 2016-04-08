39cce720766ac61be56e8c80757a7cc12624cc37
miletskiy <s.miletskiy@gmail.com>
2016-04-08 12:48:03 +0300

    Take data for 12 pokemons and change markup+js for taken data. Done 4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 
 var url = 'http://pokeapi.co/api/v1/pokemon/?limit=12';
+// var url = 'http://pokeapi.co/api/v1/pokemon/?limit=12&offset=108';
 var pokemon_data;
 
-(function takePokemons() {
-  $.getJSON(
-    url,
-    function(data) {
-      pokemon_data = data;
-    }
-  ).done(function() {
-      console.log('done');
-  });
-})()
+var $12pokemons = $('.item');
 
 var $image = $('.preview');
 var imageUrl = 'http://pokeapi.co/media/img/';
@@ -20,29 +12,61 @@ var $name = $('.details').find('h3');
 var $previewValues = $('.tableDetails td:odd');
 var pokFeatures = [];
 
-// preview single Pokemon
-$('#id_pokemon')
-  .click(function() {
-  var pokemons = pokemon_data['objects'];
-  pokemon = pokemons[1];
-  if (pokemon.types.length === 1) {
-    var types = pokemon.types[0].name;
-  }else{
-    var types = pokemon.types[0].name + ' ' + pokemon.types[1].name;
-  };
-  var attack = pokemon.attack;
-  var defense = pokemon.defense;
-  var hp = pokemon.hp;
-  var spAttack = pokemon.sp_atk;
-  var spDefense = pokemon.sp_def;
-  var speed = pokemon.speed;
-  var weight = pokemon.weight;
-  var totalMoves = pokemon.moves.length;
+var fruit = {};
+var pokemons;
 
-  pokFeatures.push(types, attack, defense, hp, spAttack,
-    spDefense,speed,weight, totalMoves);
-  takePreview(pokemons, pokemon);
-});
+(function takePokemons() {
+  $.getJSON(
+    url,
+    function(data) {
+      pokemon_data = data;
+    }
+  ).success(function() {
+      console.log('done');
+      pokemons = pokemon_data['objects'];
+
+      for(i in pokemons) {
+        pokemon = pokemons[i];
+        var pkdx_id = pokemon.pkdx_id;
+        var name = pokemon.name;
+        if (pokemon.types.length === 1) {
+          var types = pokemon.types[0].name;
+        }else{
+          var types = [];
+          types.push(pokemon.types[0].name, pokemon.types[1].name);
+        };
+        var attack = pokemon.attack;
+        var defense = pokemon.defense;
+        var hp = pokemon.hp;
+        var spAttack = pokemon.sp_atk;
+        var spDefense = pokemon.sp_def;
+        var speed = pokemon.speed;
+        var weight = pokemon.weight;
+        var totalMoves = pokemon.moves.length;
+
+        pokFeatures.push(pkdx_id, name, types, attack, defense, hp, spAttack,
+          spDefense,speed,weight, totalMoves);
+        fruit[i] = pokFeatures;
+        pokFeatures = [];
+      };
+
+  }).complete(function() {
+      console.log('complete');
+      $12pokemons.each(function(elm) {
+        $(this).find('p').text(fruit[elm][1]);
+        $(this).find('.image').attr('src', imageUrl + fruit[elm][0] + '.png');
+        var abilities = fruit[elm][2];
+        if (abilities.length === 2) {
+          $(this).find('.ability').clone().appendTo($(this));
+          $(this).find('button').addClass(abilities[0]).text(abilities[0]);
+          $(this).find('button:last').addClass(abilities[1])
+            .removeClass(abilities[0]).text(abilities[1]);
+        } else {
+          $(this).find('button').addClass(abilities).text(abilities);
+        };
+      });
+  });
+})()
 
 function takePreview(pokemons, pokemon) {
   $image.attr('src', imageUrl + pokemon.pkdx_id + '.png');
@@ -52,13 +76,15 @@ function takePreview(pokemons, pokemon) {
   });
 };
 
+// preview single Pokemon
+$('#id_pokemon')
+  .click(function() {
+  takePreview(pokemons, pokemon);
+});
+
 $('#id_loadmore')
   .click(function() {
-      var meta = pokemon_data['meta'];
-      var nextUrl = meta.next;
-      console.log('Hello!');
-      console.log(pokemons[0]);
-      console.log(nextUrl);
+      console.log($12pokemons);
   });
 
 
